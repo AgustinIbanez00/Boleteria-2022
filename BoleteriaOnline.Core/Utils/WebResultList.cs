9839a2dde9bd0e8f6398cdcbef1202afd4c3216e
Agus12344 <48695094+AgustinIbanez00@users.noreply.github.com>
2022-02-28 15:40:27 -0300

    Fix blazor destinos

---
+++ BoleteriaOnline.Core/Utils/WebResultList.cs
@@ -0,0 +1,9 @@
+﻿using BoleteriaOnline.Core.ViewModels.Pagging;
+
+namespace BoleteriaOnline.Core.Utils;
+
+public class WebResultList<T> : WebResult<T> where T : class
+{
+    public Pagination Pagging { get; set; }
+
+}
