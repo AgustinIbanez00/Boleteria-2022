b5da8337ed82ebf91655ae12d7e03d06ad162f09
Agustin Ibañez <48695094+AgustinIbanez00@users.noreply.github.com>
2022-02-28 17:57:29 -0300

    Update DistribucionEspacio.cs

--- BoleteriaOnline.Core/Data/Enums/DistribucionEspacio.cs
+++ BoleteriaOnline.Core/Data/Enums/DistribucionEspacio.cs
@@ -1,8 +1,12 @@
-﻿namespace BoleteriaOnline.Core.Data.Enums;
+namespace BoleteriaOnline.Core.Data.Enums;
 public enum DistribucionEspacio
 {
     ESPACIO_NULL = -1,
     ESPACIO_PASILLO = 1,
     ESPACIO_BUTACA = 2,
-    ESPACIO_TV = 3
+    ESPACIO_TV = 3,
+    ESPACIO_BUTACA_NO_DIPOSNIBLE = 5
+    ESPACIO_BUTACA_SEMICAMA = 6,
+    ESPACIO_BUTACA_CAMA = 7,
+    ESPACIO_ESCALERA = 8
 }
