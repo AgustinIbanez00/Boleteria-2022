bce3662c9e8c671709a5da3b105f77e3372bb1ff
Agus12344 <48695094+AgustinIbanez00@users.noreply.github.com>
2022-02-28 00:30:42 -0300

    Fix parada

--- backend/Data/Models/Parada.cs
+++ backend/Data/Models/Parada.cs
@@ -4,10 +4,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BoleteriaOnline.Web.Data.Models;
 [Index(nameof(Nombre), IsUnique = true)]
-[HumanDescription("destino", GrammaticalGender.Masculine)]
+[HumanDescription("parada", GrammaticalGender.Masculine)]
 public class Parada : AuditoryDates
 {
     public int Id { get; set; }
     [StringLength(100)]
     public string Nombre { get; set; }
+
+    public string Provincia { get; set; }
+
+    public Pais Pais { get; set; }
 }
