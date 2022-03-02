d9bfc0c79873e83edd0d05aafe2f7f49610802a9
Agus12344 <48695094+AgustinIbanez00@users.noreply.github.com>
2022-03-02 01:42:33 -0300

    Fix filtro Clientes all

--- backend/Controllers/ClientesController.cs
+++ backend/Controllers/ClientesController.cs
@@ -21,9 +21,9 @@ public class ClientesController : ControllerBase
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<ActionResult<WebResult<ICollection<ClienteDTO>>>> GetAll()
+    public async Task<ActionResult<WebResult<ICollection<ClienteDTO>>>> GetAll(ClienteFilter filter)
     {
-        var clientes = await _clienteservice.AllAsync(new ClienteFilter() { });
+        var clientes = await _clienteservice.AllAsync(filter);
 
         if (!clientes.Success)
             return StatusCode(ResponseHelper.GetHttpError(clientes.ErrorCode), clientes);
