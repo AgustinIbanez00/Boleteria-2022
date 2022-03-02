cb6736bd033453a2b5708ce7e3d4498d8709400d
lgomez <luciagomez.salusso@gmail.com>
2022-03-02 11:14:56 -0300

    Esqueleto para funcionalidad viajes

---
+++ front-end/src/app/viajes/crear-viajes/crear-viajes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-viajes',
+  templateUrl: './crear-viajes.component.html',
+  styleUrls: ['./crear-viajes.component.css']
+})
+export class CrearViajesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ front-end/src/app/viajes/filtro-viajes/filtro-viajes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filtro-viajes',
+  templateUrl: './filtro-viajes.component.html',
+  styleUrls: ['./filtro-viajes.component.css']
+})
+export class FiltroViajesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
